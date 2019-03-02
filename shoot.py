a25bc932a0512aaed02499289b10fff9cd01b69f
Christian Dohnert <donut87@gmail.com>
2019-03-02 17:20:52 +0100

    Adapting python3

--- shoot.py
+++ shoot.py
@@ -20,14 +20,14 @@ def shoot(name):
 		name = "pics/"+str(datetime.now()).replace(' ','_')+'.png'
 	last_picture = name
 	with picamera.PiCamera() as camera:
-		print "Take picture"
+		print("Take picture")
 		camera.resolution = (2592, 1944)
 		# monochrome
 		camera.color_effects = (128,128)
 		camera.contrast=contrast
 		camera.brightness = brightness
 		camera.shutter_speed=speed
-		camera.quality=100
+		#camera.quality=100
 		camera.exif_tags['IFD0.Artist'] = 'Kai Gaertner'
 		camera.exif_tags['IFD0.Copyright'] = 'Copyright (c) 2015 by Kai Gaertner'
 		# start LED lights
@@ -36,9 +36,9 @@ def shoot(name):
 		camera.capture(name)
 
 		if DEBUG:
-			print "[take_picture] Taking picture lasts "+str(time.time() - start_time)+" seconds"
+			print ("[take_picture] Taking picture lasts "+str(time.time() - start_time)+" seconds")
 		# rotate picture for processing
-		print "Rotate picture!"
+		print("Rotate picture!")
 		im = Image.open(name)
 		im.filter(ImageFilter.SHARPEN)
 		im.rotate(-90).save(name)
@@ -47,15 +47,15 @@ def shoot(name):
 def read(picture=None):
 	if DEBUG:
 		start_time = time.time()
-	print "Start OCR!"
+	print("Start OCR!")
 	if not picture:
 		picture = last_picture
 	textfile = picture.replace(".png",".txt").replace('pics/', 'texts/')
 	last_textfile = textfile
 	subprocess.call(['tesseract', picture, textfile.replace(".txt",""), '-l', 'deu'], shell=False)
-	print "TESSERACT OCR finished!"
+	print("TESSERACT OCR finished!")
 	if DEBUG:
-		print "[read] OCR analysis takes "+str(time.time() - start_time)+" seconds"
+		print("[read] OCR analysis takes "+str(time.time() - start_time)+" seconds")
 
 shoot(name)
 read(name)
